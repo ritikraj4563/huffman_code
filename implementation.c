2de5d24980a864a3d812215e5da870ccd5d54169
ritikraj4563 <106741060+ritikraj4563@users.noreply.github.com>
2024-09-06 20:27:45 +0530

    Create implementation.c

---
+++ implementation.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+int max_n; 
+// #define size 20
+
+// special character '$' for specifying internal node.
+typedef struct node_
+{
+    char ch;
+    int frequency;
+    struct node_ *left, *right;
+}node;
+
+node* create_node(char ch, int frequency)
+{
+    node *temp = (node *)(malloc(sizeof(node)));
+    temp->ch = ch;
+    temp->frequency = frequency;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+typedef struct min_heap_
+{
+    int n;
+    int capacity;
+    node **array;
+}min_heap;
+
+min_heap* create_min_heap(int size)
+{
+    min_heap *temp = (min_heap*)malloc(sizeof(min_heap));
+    temp->n = 0;
+    temp->capacity = size;
+    temp->array = (node**)(malloc(sizeof(node*)*size));
+    return temp;
+}
+void swap(node** x, node** y)
+{
+	node *temp = *x;
+    *x = *y;
+    *y = temp;
+	return;
+}
+void insert(min_heap* h)
+{
+
+	int temp = (h->n)-1;
+	int parent = (temp-1)/2;
+	while (temp != 0)
+	{
+		if (h->array[temp]->frequency < h->array[parent]->frequency)
+        {
+			swap(&(h->array[parent]), &(h->array[temp]));
+            temp = parent;
+            parent = (temp-1)/2;
+        }
+        else
+            break;
+	}
+	return;
+}
+node* delete(min_heap* h)
+{
+	swap(&(h->array[0]), &(h->array[h->n - 1]));
+	int i = 0, last_internal_node = (h->n-3)/2;
+	while (i <= last_internal_node)
+	{
+		int left_child = 2*i + 1;
+		int right_child = (2*i + 2 <= (h->n-2) ? 2*i + 2 : __INT32_MAX__);
+        int temp;
+        if (right_child == __INT32_MAX__)
+            temp = left_child;
+        else
+            temp = h->array[left_child]->frequency < h->array[right_child]->frequency ? left_child : right_child;
+		if ((h->array[i])->frequency > (h->array[temp])->frequency)
+        {
+			swap(&(h->array[i]), &(h->array[temp]));
+            i = temp;
+        }
+        else break;
+	}
+    (h->n)--;
+	return (h->array[h->n]);
+}
+min_heap* set_min_heap(min_heap *h)
+{
+    // reading file.
+	FILE *file = fopen("input4.txt", "r");
+    char ch; int fq;
+    while (fscanf(file, " %c %d", &ch, &fq) != EOF)
+    {
+        h->array[h->n] = create_node(ch, fq);
+        (h->n)++;
+        insert(h);
+    }
+    max_n = h->n;
+    return h;
+}
+void print_min_heap(min_heap *h)
+{
+    for (size_t i = 0; i < h->n; i++)
+        printf("%c %d\n", (h->array[i])->ch, (h->array[i])->frequency);
+	return ;
+}
+
+node* build_huffman_tree(min_heap *h)
+{
+    node *left, *right, *top;
+
+    while (h->n > 1)
+    {
+        left = delete(h);
+        right = delete(h);
+
+        top = create_node('$', left->frequency+right->frequency);
+        top->left = left;
+        top->right = right;
+        h->array[h->n] = top;
+        (h->n)++;
+        insert(h);
+    }
+    return delete(h);
+}
+
+void print_codes(node *root, int *array, int top)
+{
+    if (root->left)
+    {
+        array[top] = 0;
+        print_codes(root->left, array, top+1);
+        return;
+    }
+    if (root->right)
+    {
+        array[top] = 1;
+        print_codes(root->right, array, top+1);
+        return;
+    }
+    printf("%c: ", root->ch);
+    for (int i = 0; i < max_n; i++)
+        printf("%d", array[i]);
+    printf("\n");
+    return;
+}
+void huffman_code(min_heap* h)
+{
+    node *root = build_huffman_tree(h);
+    // print codes.
+    int* array = (int *)(malloc(sizeof(int)*h->capacity));
+    print_codes(root, array, 0);
+}
+int main()
+{
+    min_heap *h = create_min_heap(20);
+    set_min_heap(h);
+    print_min_heap(h);
+
+    printf("\n");
+    huffman_code(h);
+    
+	return 0;
+}
